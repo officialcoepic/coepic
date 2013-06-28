c0c20b9959a9d0cfab527b286dbd511e87e19f2e
Dhamodaran Subramanian <sdhamodaran@gmail.com>
2013-06-28 11:04:27 -0700

    new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coepic::Application.config.secret_token = '8a67a06ebded5ef3de2fbee4e3a713a7c5421d878f24374a94390289b4d11fb27c43de6273f8b461849aeb285993da2801013317a23dfe420c5ec9d9c86bc461'
